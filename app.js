11946857d1c19f6fd2a986e44150eae88593fa5f
Onwuka ekene <onwukadavid732@gmail.com>
2024-10-19 02:12:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+$("button.a").click(function() {
+    $("h1").css("color", "navy");
+});
+
+$("button.b").click(function() {
+    $("h1").css("color", "red");
+});
+
+$("button.c").click(function() {
+    $("h1").css("color", "green");
+});
+
+$("button.d").click(function() {
+    $("h1").css("color", "yellow");
+});
+
+$("button.e").click(function() {
+    $("h1").css("color", "white");
+});
+
+$("button.f").click(function() {
+    $("h1").css("color", "brown");
+});
+
+$("button.g").click(function() {
+    $("h1").css("color", "purple");
+});
+
+$("button.h").click(function() {
+    $("h1").css("color", "gold");
+});
+
+$("button.i").click(function() {
+    $("h1").css("color", "orange");
+});
+
+$("button.j").click(function() {
+    $("h1").css("color", "pink");
+});
+
+$("button.k").click(function() {
+    $("h1").css("color", "blue");
+});
+
+$("button.l").click(function() {
+    $("h1").css("color", "grey");
+});
+
+$("button.m").click(function() {
+    $("h1").css("color", "blueviolet");
+});
+
+$("button.n").click(function() {
+    $("h1").css("color", "silver");
+});
+
+$("button.o").click(function() {
+    $("h1").css("color", "chartreuse");
+});
+
+$("button.p").click(function() {
+    $("h1").css("color", "burlywood");
+});
+
+$("button.q").click(function() {
+    $("h1").css("color", "darkmagenta");
+});
+
+$("button.r").click(function() {
+    $("h1").css("color", "cyan");
+});
+
+$("button.s").click(function() {
+    $("h1").css("color", "coral");
+});
+
+$("button.t").click(function() {
+    $("h1").css("color", "darkred");
+});
+
+$("button.u").click(function() {
+    $("h1").css("color", "darkgreen");
+});
+
+$("button.v").click(function() {
+    $("h1").css("color", "darkgrey");
+});
+
+$("button.w").click(function() {
+    $("h1").css("color", "darkorange");
+});
+
+$("button.x").click(function() {
+    $("h1").css("color", "darksalmon");
+});
+
+$("button.y").click(function() {
+    $("h1").css("color", "limegreen");
+});
+
+$("button.z").click(function() {
+    $("h1").css("color", "magenta");
+});  
